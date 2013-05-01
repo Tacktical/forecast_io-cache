c0ba22a65c95a68272e058a3861d540eb9c3bada
Jon Rowe <hello@jonrowe.co.uk>
2013-05-02 09:31:56 +1000

    shortcut to sinatra app

--- lib/forecast_io/cache.rb
+++ lib/forecast_io/cache.rb
@@ -1,8 +1,14 @@
 require "forecast_io/cache/version"
+require "forecast_io/cache/app"
 
 module Forecast
   module IO
     module Cache
+
+      def self.new(*args)
+        App.new *args
+      end
+
     end
   end
 end
---
+++ lib/forecast_io/cache/app.rb
@@ -0,0 +1,9 @@
+module Forecast
+  module IO
+    module Cache
+      class App < Sinatra::Base
+
+      end
+    end
+  end
+end
