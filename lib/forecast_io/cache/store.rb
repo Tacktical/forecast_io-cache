6d57464b70fb631fe8e13b1532919ed64d25e45a
Jon Rowe <hello@jonrowe.co.uk>
2013-05-02 18:15:46 +1000

    fetching forecasts from the store

---
+++ lib/forecast_io/cache/store.rb
@@ -0,0 +1,42 @@
+require 'mongo_adaptor'
+
+module Forecast
+  module IO
+    module Cache
+      class Store
+        ForecastData = Struct.new(:position, :time, :temperature, :wind_speed, :wind_bearing,
+                                  :humidity, :pressure, :visibility, :precip_intensity)
+
+        def initialize config = Cache.configuration
+          @config = config
+          @backend = MongoAdaptor.new('forecasts',ForecastData)
+        end
+
+        def fetch lat, lon, time
+          @backend.fetch 'position' => { '$near' => [lon,lat], '$maxDistance' => @config.radius },
+                         'time' => { '$lt' => time+offset, '$gt' => time-offset }
+        rescue Mongo::OperationFailure => error
+          if index_error? error
+            ensure_index!
+            retry
+          else
+            raise
+          end
+        end
+
+        private
+          def offset
+            @config.timeframe*60
+          end
+
+          def index_error? error
+            error.message =~ /can't find any special indices/
+          end
+
+          def ensure_index!
+            MongoAdaptor.db.collection('forecasts').ensure_index position: Mongo::GEO2D
+          end
+      end
+    end
+  end
+end
---
+++ spec/forecast_io/cache/store_spec.rb
@@ -0,0 +1,33 @@
+require 'support/mongo'
+require 'forecast_io/cache/store'
+
+describe database: true do
+  let(:config) { double radius: 1, timeframe: 3 }
+  let(:store)  { Forecast::IO::Cache::Store.new config }
+  let(:mongo)  { MongoAdaptor.new('forecasts',data) }
+  let(:data)   { Forecast::IO::Cache::Store::ForecastData }
+
+  describe 'retrieving forecasts from the store' do
+    let(:forecast_1) { data.new [151.23775,-33.858264], 1 }
+    let(:forecast_2) { data.new [151.23675,-33.858664], 3 }
+
+    before do
+      mongo.insert forecast_1
+      mongo.insert forecast_2
+    end
+
+    it 'will return the closet forecast' do
+      expect(store.fetch -33.858254, 151.23775, 3).to eq forecast_1
+    end
+    it 'will return a forecast nearby' do
+      expect(store.fetch -33.858254, 151.23780, 2).to eq forecast_1
+    end
+    it 'will not return a forecast outside the time params' do
+      expect(store.fetch -33.858254, 151.23780, 200).to eq nil
+    end
+  end
+
+  describe 'inserting forecasts into the store' do
+  end
+
+end
