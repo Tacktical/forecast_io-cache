fe0daf5a9d41b54e64358ae3742887000bea0b89
Jon Rowe <hello@jonrowe.co.uk>
2013-05-02 18:28:41 +1000

    generate forecast from route

--- lib/forecast_io/cache/app.rb
+++ lib/forecast_io/cache/app.rb
@@ -5,12 +5,22 @@ module Forecast
   module IO
     module Cache
       class App < Sinatra::Base
+        LATITUDE = LONGITUDE = /(-?\d+(?:.\d+))/
+        TIME = /(\d+)/
+
+        get %r[/forecast/#{ENV['API_KEY']}/#{LATITUDE},#{LONGITUDE}(?:,#{TIME})?], provides: :json do |latitude,longitude,time|
+          respond_as_json 200, Forecast.new.for(latitude, longitude, (time || current_timestamp))
+        end
 
         not_found do
           respond_as_json 404, status: 'Not Found', actions: actions
         end
 
         private
+
+          def current_timestamp
+            Time.now.to_i
+          end
 
           def respond_as_json code, body
             [code, { "Content-Type" => "application/json" }, JSON.dump(body) ]
